1f42de75564c7681be85d8621c181b10646d8743
Ron Dronet <ron@dronetdevdesign.com>
2022-07-24 13:14:30 -0500

    finished creating the Player prototype

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -57,6 +57,7 @@ test("checks if the player is alive or not", () => {
   expect(player.isAlive()).toBeFalsy(); 
 });
 
+//players health value
 test("subtract from the players health", () => {
   const player = new Player("Dave");
   const oldHealth = player.health;
@@ -69,3 +70,38 @@ test("subtract from the players health", () => {
 
   expect(player.health).toBe(0);
 });
+
+//players attack value
+test("gets players attack value", () => {
+  const player = new Player("Dave");
+  player.strength = 10;
+
+  expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+//player potions 
+test("adds a potion to the inventory", () => {
+  const player = new Player("Dave");
+  const oldCount = player.inventory.length;
+
+  player.addPotion(new Potion());
+
+  expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+
+//select potion from inventory 
+test("uses a potion from inventory", () => {
+  const player = new Player("Dave");
+  player.inventory = [new Potion(), new Potion(), new Potion()];
+  const oldCount = player.inventory.length;
+
+  player.usePotion(1);
+
+  expect(player.inventory.length).toBeLessThan(oldCount);
+});
+
+
+
+
